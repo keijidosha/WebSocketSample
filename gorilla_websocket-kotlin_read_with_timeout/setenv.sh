4dcfb0da235dc726ef95902e3bbc0eddfd88526f
okumura <ko322jp+github@gmail.com>
2019-05-02 16:47:00 +0900

    edit classpath

--- gorilla_websocket-kotlin_read_with_timeout/setenv.sh
+++ gorilla_websocket-kotlin_read_with_timeout/setenv.sh
@@ -1,8 +1,7 @@
-export CLASSPATH=/Users/okumura/Documents/java/kotlinc/lib/kotlin-stdlib.jar
-export CLASSPATH=$CLASSPATH:/Users/okumura/Documents/java/apache-tomcat-9.0.19/lib/websocket-api.jar
-export CLASSPATH=$CLASSPATH:/Users/okumura/Documents/java/apache-tomcat-9.0.19/lib/tomcat-websocket.jar
-export CLASSPATH=$CLASSPATH:/Users/okumura/Documents/java/apache-tomcat-9.0.19/lib/tomcat-util.jar
-export CLASSPATH=$CLASSPATH:/Users/okumura/Documents/java/apache-tomcat-9.0.19/bin/tomcat-juli.jar
-export CLASSPATH=$CLASSPATH:/Users/okumura/Documents/java/apache-tomcat-9.0.19/lib/tomcat-api.jar
-export CLASSPATH=$CLASSPATH:/Users/okumura/Documents/java/kotlinlib/kotlinx-coroutines-core-1.2.1.jar
-# export CLASSPATH=$CLASSPATH:/Users/okumura/Documents/java/apache-tomcat-9.0.19/lib/tomcat-coyote.jar
+export CLASSPATH=~/Documents/java/kotlinc/lib/kotlin-stdlib.jar
+export CLASSPATH=$CLASSPATH:~/Documents/java/apache-tomcat-9.0.19/lib/websocket-api.jar
+export CLASSPATH=$CLASSPATH:~/Documents/java/apache-tomcat-9.0.19/lib/tomcat-websocket.jar
+export CLASSPATH=$CLASSPATH:~/Documents/java/apache-tomcat-9.0.19/lib/tomcat-util.jar
+export CLASSPATH=$CLASSPATH:~/Documents/java/apache-tomcat-9.0.19/bin/tomcat-juli.jar
+export CLASSPATH=$CLASSPATH:~/Documents/java/apache-tomcat-9.0.19/lib/tomcat-api.jar
+export CLASSPATH=$CLASSPATH:~/Documents/java/kotlinlib/kotlinx-coroutines-core-1.2.1.jar
